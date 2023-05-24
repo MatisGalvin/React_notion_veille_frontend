9874a9350682467d072980e17cd3099f674fb82f
MatisGalvin <72927745+MatisGalvin@users.noreply.github.com>
2023-05-24 18:56:26 +0200

    rvefcds

--- src/components/api/NotionAPI.js
+++ src/components/api/NotionAPI.js
@@ -2,7 +2,19 @@ import axios from "axios";
 
 export class NotionAPI {
   static async fetchData() {
-    const { data } = await axios.get("http://localhost:8080");
+    // Les settings de la req
+    let settings = {
+      headers: {
+        authorization:
+          "Bearer secret_o8n6JOsleFRGnEUL1flbR7jwhisq9usjVH1RR7I4X6X",
+        "notion-version": "2021-08-16",
+      },
+    };
+    const { data } = await axios.post(
+      "https://api.notion.com/v1/databases/9d4ab685f1db4798b6db0efa3cc3257b/query",
+      {},
+      settings
+    );
     console.log(data);
     return data.results;
   }
